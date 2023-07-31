65e8777e7859a5ea9a2a14dcf97935a446eb2733
JohnPosada <johnposada2000@gmail.com>
2023-07-31 11:28:46 -0500

    created base project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "dark-blue": "#2b3945",
+        "bg-darkest-blue": "#202c37",
+        "text-darkest-blue": "#111517",
+        "dark-gray": "#858585",
+        "light-gray": "#fafafa",
       },
+    },
+    screens: {
+      xs: "375px",
     },
   },
   plugins: [],
-}
+};
