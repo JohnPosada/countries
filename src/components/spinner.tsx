d6efb523c085adac48c045240a709c3df9a6f761
JohnPosada <johnposada2000@gmail.com>
2023-08-03 23:54:07 -0500

    created spinner to show while loading data

---
+++ src/components/spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="mt-6 flex h-full w-full items-center justify-center">
+      <div className="h-24 w-24 animate-spin rounded-full border-b-2 border-t-2 border-gray-300 dark:border-gray-900"></div>
+    </div>
+  );
+};
+
+export default Spinner;
