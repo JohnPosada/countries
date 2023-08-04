747299653b8a7f27ddaba076ba21f6e794183dca
JohnPosada <johnposada2000@gmail.com>
2023-08-03 23:56:01 -0500

    fixed styles

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitcher } from "./theme-swither";
 
 export const Navbar = () => {
   return (
-    <div className="h-20 bg-white w-full top-0 text-black flex justify-between px-4 items-center dark:bg-dark-blue dark:text-white">
+    <div className="top-0 flex h-20 w-full items-center justify-between bg-white px-4 text-black dark:bg-dark-blue dark:text-white">
       <h1 className="font-bold">Where in the world?</h1>
       <ThemeSwitcher />
     </div>
