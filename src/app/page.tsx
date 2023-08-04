e44c8e129a53b8a0291255f589b51f904c028beb
JohnPosada <johnposada2000@gmail.com>
2023-08-04 00:05:47 -0500

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import { InputSearch } from "@/components/input-search";
+import ListCountries from "@/components/list-countries";
+import { SearchForm } from "@/components/search-form";
 import { Navbar } from "@/components/ui/navbar";
+import { SearchProvider } from "@/context/search-context";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col bg-light-gray dark:bg-bg-darkest-blue">
-      <Navbar />
-      <InputSearch />
+      <SearchProvider>
+        <Navbar />
+        <SearchForm />
+        <ListCountries />
+      </SearchProvider>
     </main>
   );
 }
