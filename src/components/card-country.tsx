7b5053faee6520ce004862281da01d394f6a8536
JohnPosada <johnposada2000@gmail.com>
2023-09-09 11:26:30 -0500

    changed link routing

--- src/components/card-country.tsx
+++ src/components/card-country.tsx
@@ -1,7 +1,6 @@
 import { Country } from "@/types/response-country";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import type { FC } from "react";
 interface Props {
   country: Country;
@@ -10,7 +9,7 @@ interface Props {
 export const CardCountry: FC<Props> = ({ country }) => {
   return (
     <Link
-      href={`/${country.name.official.toLowerCase()}`}
+      href={`/${country.name.common.toLowerCase().replace(/\s/g, "-")}`}
       className=" flex w-[264px] cursor-pointer flex-col rounded bg-white text-text-darkest-blue shadow dark:bg-dark-blue dark:text-white"
     >
       <Image
@@ -21,7 +20,7 @@ export const CardCountry: FC<Props> = ({ country }) => {
         height={0}
       />
       <div className="flex flex-col justify-center gap-1 p-5 ">
-        <h1 className="mb-4 text-xl font-bold">{country.name.official}</h1>
+        <h1 className="mb-4 text-xl font-bold">{country.name.common}</h1>
         <p className="text-sm ">
           <span className="font-semibold">Population: </span>
           {country.population}
